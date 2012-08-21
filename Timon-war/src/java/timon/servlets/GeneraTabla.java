fc6413084eadcd3a811be2db05d535da0197e5f4
alfonso <alfonso@192.168.100.205>
2012-08-21 00:14:46 -0500

    + Listos los exportadores de resultados.

--- Timon-war/src/java/timon/servlets/GeneraTabla.java
+++ Timon-war/src/java/timon/servlets/GeneraTabla.java
@@ -17,6 +17,7 @@ package timon.servlets;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -27,15 +28,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.ClientAnchor;
 import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Drawing;
 import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Picture;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
@@ -54,23 +55,17 @@ public class GeneraTabla extends HttpServlet {
 
     @Inject
     VotoYDebateLogic vl;
+    long rsid;
+    String tipo;
+    String cual;
+    long[][] m;
+    int c;
+    List<Opcion> ops;
+    ResultadoSchulze rs;
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        long rsid;
-        String tipo;
-        String cual;
         try {
             rsid = Integer.parseInt(request.getParameter("rsid")); // ResultadoSchulze
             tipo = request.getParameter("tipo"); // Tipo de archivo
@@ -79,13 +74,12 @@ public class GeneraTabla extends HttpServlet {
             throw new ServletException("Faltan parámetros de ejecución.");
         }
 
-        ResultadoSchulze rs = vl.getResultadoSchulze(rsid);
+        rs = vl.getResultadoSchulze(rsid);
         if (rs == null) {
             throw new ServletException("Al parecer no existe un resultado con esa clave.");
         }
-        int c = rs.getVotacion().getOpciones().size();
-        long[][] m;
-        List<Opcion> ops = rs.getVotacion().getOpciones();
+        c = rs.getVotacion().getOpciones().size();
+        ops = rs.getVotacion().getOpciones();
         for (Opcion o : ops) {
             System.out.println("Opcion: " + o.getNombre());
         }
@@ -111,22 +105,83 @@ public class GeneraTabla extends HttpServlet {
                 throw new ServletException("No entiendo el tipo de archivo.");
         }
 
+
+
+    }
+
+    private int getPictureType(byte[] f) {
+        byte[] png = new byte[]{(byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A};
+        byte[] jpg = new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF};
+        if (Arrays.equals(Arrays.copyOfRange(f, 0, 8), png)) {
+            return Workbook.PICTURE_TYPE_PNG;
+        }
+        if (Arrays.equals(Arrays.copyOfRange(f, 0, 3), jpg)) {
+            return Workbook.PICTURE_TYPE_JPEG;
+        }
+        return Workbook.PICTURE_TYPE_PNG; //default
+    }
+
+    private void regresaCsv(HttpServletResponse response) throws IOException {
+        StringBuilder out = new StringBuilder();
+        out.append(",");
+        for (int i = 0; i < c; i++) {
+            out.append(ops.get(i).getNombre());
+            if (i != (c - 1)) {
+                out.append(",");
+            }
+        }
+        out.append("\n");
+        for (int i = 0; i < c; i++) {
+            out.append(ops.get(i).getNombre()).append(",");
+            for (int j = 0; j < c; j++) {
+                if (!ops.get(i).equals(ops.get(j))) {
+                    out.append(m[i][j]);
+                    if (j != (c - 1)) {
+                        out.append(",");
+                    }
+                } else {
+                    out.append(",");
+                }
+            }
+            out.append("\n");
+        }
+        String mat="";
+        if (cual.equals("pref")) {
+            mat = " - Matriz de Preferencia.csv";
+        }
+        if (cual.equals("sp")) {
+            mat = " - Rutas Más Fuertes.csv";
+        }
+        
+        response.setHeader("Content-Disposition", "attachment; filename=\"Resultados " + rs.getVotacion().getNombre() + mat+"\"");
+        response.setContentType("application/csv");        
+        PrintWriter o = response.getWriter();
+        o.write(out.toString());
+    }
+
+    private void regresaExcel(HttpServletResponse response) throws ServletException, IOException {
+
         XSSFColor rojo = new XSSFColor(new byte[]{(byte) 0xFF, (byte) 0xDD, (byte) 0xDD});
         XSSFColor verde = new XSSFColor(new byte[]{(byte) 0xDD, (byte) 0xFF, (byte) 0xDD});
         XSSFColor gris = new XSSFColor(new byte[]{(byte) 0xDD, (byte) 0xDD, (byte) 0xDD});
-
         Workbook wb = new SXSSFWorkbook(c);
         XSSFFont bold = (XSSFFont) wb.createFont();
         bold.setBold(true);
+        XSSFFont vals = (XSSFFont) wb.createFont();
+        vals.setFontHeight(24);
         XSSFCellStyle headstyle = (XSSFCellStyle) wb.createCellStyle();
         headstyle.setFont(bold);
+        headstyle.setAlignment(HorizontalAlignment.CENTER);
         XSSFCellStyle headstyler = (XSSFCellStyle) wb.createCellStyle();
         headstyler.setAlignment(XSSFCellStyle.ALIGN_RIGHT);
         headstyler.setFont(bold);
+        headstyler.setAlignment(HorizontalAlignment.CENTER);
+        headstyler.setVerticalAlignment(VerticalAlignment.CENTER);
         XSSFCellStyle celrojo = (XSSFCellStyle) wb.createCellStyle();
         celrojo.setFillForegroundColor(rojo);
         celrojo.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         celrojo.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+        celrojo.setVerticalAlignment(VerticalAlignment.CENTER);
         celrojo.setBorderTop(XSSFCellStyle.BORDER_THIN);
         celrojo.setBorderLeft(XSSFCellStyle.BORDER_THIN);
         celrojo.setBorderRight(XSSFCellStyle.BORDER_THIN);
@@ -135,11 +190,12 @@ public class GeneraTabla extends HttpServlet {
         celrojo.setLeftBorderColor(gris);
         celrojo.setRightBorderColor(gris);
         celrojo.setBottomBorderColor(gris);
-
+        celrojo.setFont(vals);
         XSSFCellStyle celverde = (XSSFCellStyle) wb.createCellStyle();
         celverde.setFillForegroundColor(verde);
         celverde.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         celverde.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+        celverde.setVerticalAlignment(VerticalAlignment.CENTER);
         celverde.setBorderTop(XSSFCellStyle.BORDER_THIN);
         celverde.setBorderLeft(XSSFCellStyle.BORDER_THIN);
         celverde.setBorderRight(XSSFCellStyle.BORDER_THIN);
@@ -148,18 +204,19 @@ public class GeneraTabla extends HttpServlet {
         celverde.setLeftBorderColor(gris);
         celverde.setRightBorderColor(gris);
         celverde.setBottomBorderColor(gris);
+        celverde.setFont(vals);
         CreationHelper helper = wb.getCreationHelper();
 
         Sheet sh = wb.createSheet("Matriz de Preferencia");
         sh.setDisplayGridlines(false);
         Row head = sh.createRow(5);
-        
+
         for (int i = 0; i < c; i++) {
             Cell cell = head.createCell(i + 2);
-            
+
             cell.setCellStyle(headstyle);
             cell.setCellValue(ops.get(i).getNombre());
-            sh.setColumnWidth(1+2, 3500);
+            sh.setColumnWidth(i + 2, 3500);
             if (vl.tieneImagenLaOpcion(ops.get(i).getId())) {
                 byte[] ima = vl.getImagenDeOpcion(ops.get(i).getId());
                 int pictureid = wb.addPicture(ima, getPictureType(ima));
@@ -171,15 +228,14 @@ public class GeneraTabla extends HttpServlet {
                 ByteArrayInputStream in = new ByteArrayInputStream(ima);
                 BufferedImage bima = ImageIO.read(in);
                 int w = bima.getWidth();
-
-                float factor = 90f / w;
-                System.out.println("W: " + w + " Factor: " + factor);
-
+                float factor = 80f / w;
                 pict.resize(factor);
             }
         }
+
         for (int i = 0; i < c; i++) {
             Row row = sh.createRow(i + 6);
+            row.setHeightInPoints((4 * sh.getDefaultRowHeightInPoints()));
             Cell nom = row.createCell(1);
             nom.setCellValue(ops.get(i).getNombre());
             nom.setCellStyle(headstyler);
@@ -196,32 +252,12 @@ public class GeneraTabla extends HttpServlet {
             }
         }
         sh.autoSizeColumn(1);
-        response.setHeader("Content-Disposition", "attachment; filename=\"Resultados " + rs.getVotacion().getNombre() + ".xlsx\"");
+        sh.setColumnWidth(0, 5500);
 
+        response.setHeader("Content-Disposition", "attachment; filename=\"Resultados " + rs.getVotacion().getNombre() + ".xlsx\"");
         response.setContentType("application/xlsx");
         wb.write(response.getOutputStream());
 
-
-
-    }
-
-    private int getPictureType(byte[] f) {
-        byte[] png = new byte[]{(byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A};
-        byte[] jpg = new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF};
-
-        if (Arrays.equals(Arrays.copyOfRange(f, 0, 8), png)) {
-            return Workbook.PICTURE_TYPE_PNG;
-        }
-        if (Arrays.equals(Arrays.copyOfRange(f, 0, 3), jpg)) {
-            return Workbook.PICTURE_TYPE_JPEG;
-        }
-        return Workbook.PICTURE_TYPE_PNG; //default
-    }
-
-    private void regresaCsv(HttpServletResponse response) {
-    }
-
-    private void regresaExcel(HttpServletResponse response) {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
